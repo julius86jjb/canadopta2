bfb4ff2bc26374c3502eb9ab38a4a4296191a0b9
Julio Jimenez <julius.1986@gmail.com>
2019-07-14 10:33:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdopcionesComponent } from './pages/adopciones/adopciones.component';
+import { CentrosComponent } from './pages/centros/centros.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BigfooterComponent } from './shared/bigfooter/bigfooter.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
+import { APP_ROUTES } from './app.routes';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    HomeComponent,
+    AdopcionesComponent,
+    CentrosComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BigfooterComponent,
+    FooterComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes, RouterModule } from '@angular/router';
+
+// import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: '', redirectTo: '/home', pathMatch: 'full' }
+        ]
+    },
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent  },
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, {useHash: true});
